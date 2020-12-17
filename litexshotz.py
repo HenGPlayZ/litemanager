f4fb530af3e2f7e617ed195c688c9a2d4043540c
HenGPlayZ <76139625+HenGPlayZ@users.noreply.github.com>
2020-12-17 16:33:54 +0700

    Update litexshotz.py

--- litexshotz.py
+++ litexshotz.py
@@ -6,7 +6,7 @@ client = commands.Bot(command_prefix = '.')
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Activity(
-        type=discord.ActivityType.watching, name="pornhub,com"
+        type=discord.ActivityType.streaming, name="pornhub,com"
             ))
     print("Ready")
 
